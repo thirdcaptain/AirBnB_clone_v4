ea146eb9fa88d4da0c5371a95d29dcb50336306d
Yunju Chen <info@yunjuchen.net>
2018-05-11 23:09:46 +0000

    Add amenity name and id

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,8 +1,19 @@
-$('button').click(function () {
-  console.log('hello');
-  if ($('#amenity').is(':checked')) {
-    
-  } else {
-  
-  }
+$( document ).ready(function () {
+  let id_dict = {};
+  $('input:checkbox').change(function() {
+    if ($(this).is(':checked')) {
+      id_dict[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else {
+      delete id_dict[$(this).attr('data-name')];
+    }
+    let list = [];
+    $.each(id_dict, function(index, value){
+      list.push(index);
+    });
+    if (list.length === 0) {
+      $('.amenities h4').text("");
+    } else {
+      $('.amenities h4').text(list.join(', '));
+    }
+  })
 });
