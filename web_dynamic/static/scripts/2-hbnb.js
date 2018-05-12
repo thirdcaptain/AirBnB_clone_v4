2c0117c9d9bf3594f7494c4bfd801415875e043d
Robert MALMSTEIN <156@p2-4.hbtn.io>
2018-05-12 10:58:01 -0700

    Add a get request to our API

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  let idDict = {};
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      idDict[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else {
+      delete idDict[$(this).attr('data-name')];
+    }
+    let list = [];
+    $.each(idDict, function (index, value) {
+      list.push(index);
+    });
+    if (list.length === 0) {
+      $('.amenities h4').text('');
+    } else {
+      $('.amenities h4').text(list.join(', '));
+    }
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status', function (data) {
+    if (data['status'] === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  }, 'json');
+});
